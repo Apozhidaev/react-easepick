13e4ea95c698188cd7fbbc56539fd0f5d334477a
alexanderpo <alexander.pozhidaev@gmail.com>
2022-12-20 16:16:48 +0100

    hotfix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import type { EasePickOptions, EasePickInputElement } from "easepick-react";
+import type { IAmpPlugin } from "@easepick/amp-plugin/dist/interface";
 import EasePicker from "easepick-react";
 
-export type { EasePickOptions, EasePickInputElement };
+export * from "@easepick/bundle";
+
+type Options = EasePickOptions & { AmpPlugin?: IAmpPlugin };
+
+export type { Options as EasePickOptions, EasePickInputElement };
 
 export default EasePicker;
