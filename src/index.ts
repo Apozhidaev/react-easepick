0ff74b7eec490798f33af6153e60767681bdcdb5
alexanderpo <alexander.pozhidaev@gmail.com>
2022-12-20 18:50:03 +0100

    hotfix

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
 import type { EasePickOptions, EasePickInputElement } from "easepick-react";
 import type { IAmpPlugin } from "@easepick/amp-plugin/dist/interface";
 import EasePicker from "easepick-react";
-
-export * from "@easepick/bundle";
+import {
+  AmpPlugin,
+  DateTime,
+  KbdPlugin,
+  LockPlugin,
+  PresetPlugin,
+  RangePlugin,
+  TimePlugin,
+} from "@easepick/bundle";
 
 type Options = EasePickOptions & { AmpPlugin?: IAmpPlugin };
 
 export type { Options as EasePickOptions, EasePickInputElement };
+
+export {
+  AmpPlugin,
+  DateTime,
+  KbdPlugin,
+  LockPlugin,
+  PresetPlugin,
+  RangePlugin,
+  TimePlugin,
+};
 
 export default EasePicker;
